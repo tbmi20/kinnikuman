6c105edf1666ee363c6c4d6092cc9eca4b8f2ea9
tbmi20 <tbmi20@bath.ac.uk>
2024-06-03 11:14:55 +0100

    started button component

--- app/workout/page.tsx
+++ app/workout/page.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
+
 const WorkoutPage = () => {
     return (
         <div>
             <h1>Workout</h1>
-            <button onClick={() => console.log('Start Workout')}>Start Workout</button>
-            <button onClick={() => console.log('Create Template')}>Create Template</button>
         </div>
     );
 };
