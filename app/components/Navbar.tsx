4f958751c0982923eb0bd87274ff6441815b0364
tbmi20 <tbmi20@bath.ac.uk>
2024-07-24 23:40:39 +0100

    Made the items horizontal

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Logo from './Logo';
 
 export default function Navbar() {
     return (
-        <div className='fixed top-0 left-0 h-16 w-screen m-0 flex flex-col text-justify bg-blue-300 text-white shadow-lg'>
+        <div className='fixed top-0 left-0 h-16 w-screen m-0 flex flex-row text-justify bg-blue-300 text-white shadow-lg'>
             <Logo></Logo>
             <Link href="app\account">Account</Link>
             <Link href="app\Diary">Diary</Link>
