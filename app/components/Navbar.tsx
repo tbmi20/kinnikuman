5be74d8ce1f06d8b05eedbebbfd76e0b759771f6
tbmi20 <tbmi20@bath.ac.uk>
2024-07-25 19:41:48 +0100

    Added Icons to navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,34 @@
 import Link from 'next/link';
 import Logo from './Logo';
+import { IconType } from 'react-icons';
+import { FaDumbbell } from "react-icons/fa";
+import { IoPerson } from "react-icons/io5";
+import { FaBook } from "react-icons/fa6";
+
+
 //style with tailwindcss
 
 export default function Navbar() {
     return (
-        <div className='fixed top-0 left-0 h-16 w-screen m-0 flex flex-row text-justify bg-blue-300 text-white shadow-lg'>
+        <div className='fixed bottom-0 left-0 h-16 w-screen m-0 flex flex-row text-justify bg-blue-300 text-white shadow-lg'>
             <Logo></Logo>
-            <Link href="app\account">Account</Link>
-            <Link href="app\Diary">Diary</Link>
-            <Link href="app\Workout">Workout</Link>
+            <NavIcon icon={<FaBook size="28"/>} tooltip='Diary'/>
+            <NavIcon icon={<FaDumbbell size="28"/>} tooltip='Workouts'/>
+            <NavIcon icon={<IoPerson size="28"/>} tooltip='Account'/>
         </div>
+    );
+}
+
+interface NavIconProps {
+    icon: React.ReactNode;
+    tooltip?: string;
+}
+
+function NavIcon({ icon, tooltip = 'temp' }: NavIconProps) {
+    return (
+    <div className='navbar-icon group'>
+        {icon}
+        <div className='navbar-tooltip group-hover:scale-50'>{tooltip}</div>
+    </div>
     );
 }
