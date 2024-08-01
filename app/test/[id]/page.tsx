e551705ac86e2dc6d2a2c5e16db38ef0092f955b
Takuma <tbmi20@bath.ac.uk>
2024-08-01 22:33:19 +0100

    set up mongoDB

--- app/test/[id]/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Page({ params }: {params: {id: string}}) {
-    return (
-        <div>
-            <h1>Account {params.id}</h1>
-            <p>This is the account page</p>
-        </div>
-    );
-}
