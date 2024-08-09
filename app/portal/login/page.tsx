80cfb850c37c50208becfa21de2bb06ba02647e0
Takuma <tbmi20@bath.ac.uk>
2024-08-09 13:26:05 +0100

    edited getExercise

--- app/portal/login/page.tsx
+++ app/portal/login/page.tsx
@@ -3,10 +3,6 @@ export default function loginPage() {
       <div className="bg-gray-50 font-[sans-serif]">
     <div className="min-h-screen flex flex-col items-center justify-center py-6 px-4">
       <div className="max-w-md w-full">
-        <a href="javascript:void(0)"><img
-          src="https://readymadeui.com/readymadeui.svg" alt="logo" className='w-40 mb-8 mx-auto block' />
-        </a>
-
         <div className="p-8 rounded-2xl bg-white shadow">
           <h2 className="text-gray-800 text-center text-2xl font-bold">Sign in</h2>
           <form className="mt-8 space-y-4">
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    env: {
+    MONGODB_URI: "mongodb+srv://tbmartinito:f7W87UsoyzInyCj9@kinnikumanm0.utrobfv.mongodb.net/",
+    FITNESS_DB: "fitnessApp"
+    },
+};
 
 export default nextConfig;
