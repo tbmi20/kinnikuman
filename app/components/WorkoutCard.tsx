0db5ef70a4e6e56c99db088c051ae043b2bdff1e
Takuma <tbmi20@bath.ac.uk>
2024-08-14 20:11:17 +0100

    trying to switch to tsx

--- app/components/WorkoutCard.tsx
+++ app/components/WorkoutCard.tsx
@@ -1,9 +1,18 @@
 'use client';
 import { IoCloseOutline } from "react-icons/io5";
 
-export default function WorkoutCard() {
+interface WorkoutCardProps {
+    workout: Array<string>;
+}
+
+export default function WorkoutCard({ workout }: WorkoutCardProps) {
     return (
         <div className="size-52 mx-5 rounded-md bg-blue_1 p-3">
+            <ul>
+                {workout.map((item, index) => (
+                    <li key={index}>{item}</li>
+                ))}
+            </ul>
             <StartWorkout></StartWorkout>
         </div>
     );
