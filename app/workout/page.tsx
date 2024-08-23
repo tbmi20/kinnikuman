510f4e43512baeaa3b1d1c59a536d957d38f1852
Takuma <tbmi20@bath.ac.uk>
2024-08-23 23:00:19 +0100

    reverse

--- app/workout/page.tsx
+++ app/workout/page.tsx
@@ -3,7 +3,7 @@ import WorkoutCard from '../components/WorkoutCard';
 import Navbar from '../components/Navbar';
 import Background from '../components/Background';
 import getWorkouts from '../../lib/mongo/workouts.js';
-/////
+////
 export default function WorkoutPage() {
     return (
         <div>
