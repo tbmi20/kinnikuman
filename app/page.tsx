38bef6d2eb438195b288107e954f118c32651bef
Takuma <tbmi20@bath.ac.uk>
2024-09-14 22:40:39 +0100

    implemented shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,31 @@
-import Link from 'next/link';
-import Navbar from './components/Navbar';
-import Background from './components/Background';
+import Link from "next/link";
+import Navbar from "./components/Navbar";
+import Background from "./components/Background";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 export default function HomePage() {
   return (
     <div>
       <Background></Background>
       <h1>Dashboard</h1>
+      <Carousel>
+        <CarouselContent>
+          <CarouselItem>...</CarouselItem>
+          <CarouselItem>...</CarouselItem>
+          <CarouselItem>...</CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+
       <Link href="/workout">Workout</Link>
       <Navbar></Navbar>
     </div>
   );
-} 
+}
