091e57124b4fa42bf1d4a1553ad3799d2246d02b
Takuma <tbmi20@bath.ac.uk>
2024-09-24 20:43:29 +0100

    fixed env

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,7 +1,6 @@
 import Navbar from "../components/Navbar";
 import Background from "../components/Background";
 import Header from "../components/Header";
-import { Profile } from "@/app/api/session";
  
  export default function Account() {
     return (
@@ -10,7 +9,6 @@ import { Profile } from "@/app/api/session";
             <Header page="Account"></Header>
             <div>
                 Welcome
-                <Profile></Profile>
             </div>
             <Navbar></Navbar>
         </div>
