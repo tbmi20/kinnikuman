c21124ee8a9bf3445f2cfbc01fdf2f3b470bc441
Takuma <tbmi20@bath.ac.uk>
2024-09-25 17:15:08 +0100

    testing db

--- app/workout/page.tsx
+++ app/workout/page.tsx
@@ -1,22 +1,34 @@
-import React from 'react';
-import WorkoutCard from '../components/WorkoutCard';
-import Navbar from '../components/Navbar';
-import Background from '../components/Background';
-import Header from '../components/Header';
+import React from "react";
+import WorkoutCard from "../components/WorkoutCard";
+import Navbar from "../components/Navbar";
+import Background from "../components/Background";
+import Header from "../components/Header";
+import clientPromise from "@/lib/mongodb";
 
 ////
 export default function WorkoutPage() {
-    return (
-        <div>
-            <Background></Background>
-            <Header page="Workout"></Header>
-            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4'>
-                <WorkoutCard/>
-                <WorkoutCard/>
-                <WorkoutCard/>
-            </div>
-            <Navbar></Navbar>
-        </div>
-        
-    );
-};
+  return (
+    <div>
+      <Background></Background>
+      <Header page="Workout"></Header>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 p-4">
+        <WorkoutCard />
+        <WorkoutCard />
+        <WorkoutCard />
+      </div>
+      <div></div>
+      <TestExercise/>
+      <Navbar></Navbar>
+    </div>
+  );
+}
+
+async function TestExercise() {
+  const client = await clientPromise;
+  const db = client.db("fitnessApp");
+  const exercise = await db
+    .collection("exercises")
+    .findOne({ name: "Bench Press" });
+
+  return <div>{exercise!.name}</div>;
+}
